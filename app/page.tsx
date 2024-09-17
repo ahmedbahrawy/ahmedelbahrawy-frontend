84b2896bbb44321e35fe1a82a5b3be57c21a365a
Ahmed Elbahrawy <ahmedbahrawyqa@gmail.com>
2024-09-17 13:07:02 +0200

    Setup project with shadcn and ace

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Progress } from "@/components/ui/progress";
+import { Input } from "@/components/ui/input";
+import { ProductItems } from "@/components/product/product-items";
+import { HomeGlob } from "@/components/home/home-glob";
+
+export default function Home() {
+  return (
+    <div className="">
+      <HomeGlob />
+    </div>
+  );
+}
